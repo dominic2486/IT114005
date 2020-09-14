73ee8e6a3a07f6bad186439e773c2d34f8acfa37
dominic2486 <dwq2@njit.edu>
2020-09-14 17:13:59 -0400

    Upload IT114- IT HW

---
+++ IT114/src/Homework.java
@@ -0,0 +1,22 @@
+import static java.lang.System.out;
+
+public class Homework 
+{
+	public static void main(String[] args) 
+	{
+		// If it matches my sample exactly it gets a 0
+		// 1. Create a variable that holds a name
+		String name="dominic";
+		// 2. Output the name to the console
+		out.println(name);
+		// 3. Create a variable that holds an age (as a whole number)
+		int age = 20;
+		// 4. Output the age to the console
+		out.println(age);
+		// 5. Create a variable that holds a decimal
+		double dec=34.76;
+		// 6. Output the variable
+		out.println(dec);
+	}
+
+}
