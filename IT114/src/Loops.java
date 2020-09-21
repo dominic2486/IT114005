b54401ae3f0d352c65555abd61f5fc31aff24b14
dominic2486 <dwq2@njit.edu>
2020-09-21 15:46:37 -0400

    adding Loops HW

---
+++ IT114/src/Loops.java
@@ -0,0 +1,24 @@
+import static java.lang.System.out;
+
+public class Loops 
+{
+	public static void main(String[] args) 
+	{
+		//out.println("1.");
+		int nums[] = {1,2,4,5,6,7};
+		//out.println("2.");
+		for(int i=0;i<nums.length;i++)
+		{
+			out.println("nums["+i+"] = "+nums[i]);
+		}
+		out.println("3.");
+		for(int i=0;i<nums.length;i++)
+		{
+			if(nums[i]%2==0)
+				out.println("nums["+i+"] = "+nums[i]);
+			
+		}
+
+	}
+
+}
